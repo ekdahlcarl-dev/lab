22a1008b13b69f0e5cffee3bbae5ebc940f1eb6c
ekdahlcarl-dev <ekdahl.carl@gmail.com>
2026-08-06 13:33:09 +0200

    Add checkout component structure

---
+++ components/ProductSummary.tsx
@@ -0,0 +1,10 @@
+export default function ProductSummary() {
+  return (
+    <div className="border rounded-xl p-4 mb-6">
+      <div className="flex justify-between">
+        <span>Product</span>
+        <strong>499 SEK</strong>
+      </div>
+    </div>
+  );
+}
