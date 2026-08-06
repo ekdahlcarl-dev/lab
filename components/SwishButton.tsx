9a14a02790e8e9bb24a57f0db630b01f5ae68caf
ekdahlcarl-dev <ekdahl.carl@gmail.com>
2026-08-06 13:33:18 +0200

    Add Swish payment component

---
+++ components/SwishButton.tsx
@@ -0,0 +1,7 @@
+export default function SwishButton() {
+  return (
+    <button className="w-full bg-green-600 text-white rounded-xl py-3 font-semibold">
+      Pay with Swish
+    </button>
+  );
+}
