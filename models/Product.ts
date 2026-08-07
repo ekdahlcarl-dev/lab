16ad9779e412f97b3cf64ab1d3ab90b85bcc6813
ekdahlcarl-dev <ekdahl.carl@gmail.com>
2026-08-07 08:15:45 +0200

    LAB-5 add product domain model

---
+++ models/Product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+  id: string;
+  name: string;
+  description: string;
+  price: number;
+  currency: string;
+  categoryId: string;
+  image: string;
+  available: boolean;
+}
